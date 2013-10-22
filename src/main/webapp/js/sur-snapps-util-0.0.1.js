6441c89e662552034c9ceb4ad98d1690f0759c38
soezen <rogge.suzan@gmail.com>
2013-10-22 15:46:05 +0200

    add util.Timer tests

--- src/main/webapp/js/sur-snapps-util-0.0.1.js
+++ src/main/webapp/js/sur-snapps-util-0.0.1.js
@@ -23,27 +23,83 @@ sur.snapps.util.isUndefined = function (object) {
 sur.snapps.util.isFunction = function (object) {
     return !sur.snapps.util.isUndefined(object)
         && typeof object === 'function';
-}
+};
+
+/**
+ *
+ * @param object
+ * @returns {boolean} true if object is a number
+ */
+sur.snapps.util.isNumber = function (object) {
+    return !sur.snapps.util.isUndefined(object)
+        && typeof object === 'number';
+};
 
 // TIMER
 /**
- * Create Timer Object, when started, counts until it has reached its limit (if one was specified, otherwise counts until infinity). It is also possible to pause or stop the timer, increase or decrease the limit.
+ * Create Timer Object,
+ * when started, counts until it has reached its limit (if one was specified,
+ * otherwise counts until infinity). It is also possible to pause or stop
+ * the timer, increase or decrease the limit.
  *
- * @param field field in which the current time will be displayed
+ * @param limit limit until which the timer counts
  * @param limitCallback called when the limit is reached
  * @param countCallback called on every count
  * @param callbackObject object which contains the callback function
  * @constructor
  */
-sur.snapps.util.Timer = function (limit, limitCallback, countCallback, callbackObject) {
+sur.snapps.util.Timer = function Timer(limit, limitCallback, countCallback, callbackObject) {
+    if (!(this instanceof Timer)) {
+        return new Timer(limit, limitCallback, countCallback, callbackObject);
+    }
+
     var time = -1;
     var running = false;
-    var withLimit = !sur.snapps.util.isUndefined(limit);
+    limit = sur.snapps.util.isNumber(limit) ? limit : false;
+    countCallback = sur.snapps.util.isFunction(countCallback) ? countCallback : false;
 
-    this.count = function () {
+    function count() {
         time += 1;
-        if (sur.snapps.util.isFunction(countCallback)) {
-            countCallback.call(callbackObject);
+
+        if (limit && time >= limit) {
+            limitCallback.call(callbackObject);
+            running = false;
         }
+
+        setTimeout(function () {
+            if (running) {
+                if (countCallback) {
+                    countCallback.call(callbackObject);
+                }
+                count();
+            }
+        }, 1000);
     }
+
+    this.current = function () {
+        return time;
+    };
+
+    this.isRunning = function () {
+        return running;
+    };
+
+    this.start = function () {
+        time = -1;
+        running = true;
+        count();
+    };
+
+    this.stop = function () {
+        running = false;
+    };
+
+    this.increaseLimit = function (addLimit) {
+        limit += addLimit;
+    };
+
+    this.decreaseLimit = function (removeLimit) {
+        limit -= removeLimit;
+        limit = limit < 0 ? -1 : limit;
+    };
 };
--- src/main/webapp/unittests/spec/sur-snapps-util-spec.js
+++ src/main/webapp/unittests/spec/sur-snapps-util-spec.js
@@ -33,3 +33,112 @@ describe("Util.isFunction()", function () {
         expect(callTestFunction("")).toBe(false);
     });
 });
+
+describe("Util.isNumber()", function () {
+    function callTestFunction(param) {
+        return sur.snapps.util.isNumber(param);
+    }
+
+    it("returns false if object is undefined", function () {
+        expect(callTestFunction(undefined)).toBe(false);
+    });
+    it("returns false if object is not a number", function () {
+        expect(callTestFunction("")).toBe(false);
+    });
+    it("returns true if object is a number", function () {
+        expect(callTestFunction(1)).toBe(true);
+    });
+});
+
+describe("Util.Timer()", function () {
+
+    var limitCallbackHits;
+    var countCallbackHits;
+    var timer;
+
+    beforeEach(function () {
+        limitCallbackHits = 0;
+        countCallbackHits = 0;
+        var limitCallback = function () {
+            limitCallbackHits += 1;
+        };
+        var countCallback = function () {
+            countCallbackHits += 1;
+        };
+        timer = new sur.snapps.util.Timer(5, limitCallback, countCallback);
+        jasmine.Clock.useMock();
+    });
+
+    afterEach(function () {
+        timer.stop();
+    });
+
+    it("returns a timer object when using new", function () {
+        expect(typeof new sur.snapps.util.Timer()).toBe('object');
+        expect(typeof sur.snapps.util.Timer()).toBe('object');
+    });
+
+    describe("start function", function () {
+        it("starts the timer", function () {
+            timer.start();
+            expect(timer.isRunning()).toBe(true);
+            jasmine.Clock.tick(2000);
+            expect(timer.current()).toBeGreaterThan(0);
+        });
+        it("restarts when called again", function () {
+            timer.start();
+            jasmine.Clock.tick(3000);
+
+            var current = timer.current();
+            timer.start();
+            var newCurrent = timer.current();
+            expect(newCurrent).toBeLessThan(current);
+        });
+    });
+
+    describe("stop function", function () {
+        it("stops the timer if timer is running", function () {
+            timer.start();
+            expect(timer.isRunning()).toBe(true);
+            timer.stop();
+            expect(timer.isRunning()).toBe(false);
+        });
+        it("does nothing if timer is not running", function () {
+            timer.stop();
+            expect(timer.isRunning()).toBe(false);
+        });
+    });
+
+    describe("increase limit function", function () {
+        it("increases the limit of the timer", function () {
+            timer.start();
+            timer.increaseLimit(5);
+            jasmine.Clock.tick(12000);
+
+            expect(timer.isRunning()).toBe(false);
+            expect(timer.current()).toBe(10);
+            expect(limitCallbackHits).toBe(1);
+            expect(countCallbackHits).toBe(10);
+        });
+        it("initializes the limit of the timer if non was set yet", function () {
+            timer = new sur.snapps.util.Timer("test");
+            timer.increaseLimit(5);
+            timer.start();
+            jasmine.Clock.tick(7000);
+
+            expect(timer.current()).toBe(5);
+
+        });
+    });
+
+    describe("decrease limit function", function () {
+        it("decreases the limit of the timer", function () {
+            timer.decreaseLimit(3);
+            timer.start();
+            jasmine.Clock.tick(5000);
+
+            expect(timer.isRunning()).toBe(false);
+            expect(timer.current()).toBe(2);
+        });
+    });
+});
